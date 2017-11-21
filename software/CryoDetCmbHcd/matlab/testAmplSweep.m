aaadfb5383903b9e561254a90d234df8344a389b
ssmith432 <ssmith@slac.stanford.edu>
2017-11-21 12:24:42 -0800

    new & improved

--- software/CryoDetCmbHcd/matlab/testAmplSweep.m
+++ software/CryoDetCmbHcd/matlab/testAmplSweep.m
@@ -1,14 +1,22 @@
 %script testAmplSweep
 
 Nread = 4
-dwell = 0.001
-freqs = -9.6e6:1.2e6:9.6e6;
+dwell = 0.02
+freqs = -19:1:19;   %frequencies in MHz
+
+
 
 for band=0:7
-    resp = amplSweep(band, freqs, Nread, dwell);
-    figure(band+1);plot(freqs/1e6, abs(resp));grid
+    [resp, f] = amplSweep(band, freqs, Nread, dwell);
+    display(['for band ', num2str(band), ' stDev = ', num2str(std(resp),5)])
+    figure(band+1);plot(f, abs(resp), '.');grid
     title(['Amplitude Response for Band ' num2str(band)])
-    xlabel('Frequency (MHz)')git ci 
+    xlabel('Frequency (MHz)')
+    ylabel('Response (arbs)')
+    
+    figure(100), subplot(2, 4, band+1), plot(f, abs(resp), '.');grid
+    title(['Amplitude Response for Band ' num2str(band)])
+    xlabel('Frequency (MHz)')
     ylabel('Response (arbs)')
 end
 

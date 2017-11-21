14a7c689628f59b53e447c2e195db433a784bc3d
ssmith432 <ssmith@slac.stanford.edu>
2017-11-21 12:24:28 -0800

    new & improved

--- software/CryoDetCmbHcd/matlab/amplSweep.m
+++ software/CryoDetCmbHcd/matlab/amplSweep.m
@@ -1,8 +1,8 @@
-function resp = amplSweep(band, freqs, Nread, dwell)
+function [resp, f] = amplSweep(band, freqs, Nread, dwell)
 % Sweep frequency, plot amplitude and phase
 % band selects one of 32 sub bands (only 0:7 presently allowed)
-% freqs is a vector of frequencies in the range -19.2 MHz to + 19.2 MHz
-% in units of fractions of 19.2 MHz
+% freqs is a vector of frequencies in the range -19.2 (MHz) to + 19.2 (MHz)
+
 % resp is complex response demodulated response
 % Nread (optional) number of reads of response per frequency setting
 % dwell (optional) dwell time between setting and read and between reads
@@ -36,38 +36,40 @@ lcaPut( [rootPath, 'feedbackEnable'], 0 ) %Disable FB
 
 % choose drive amplitude
 Adrive = 12; % -6 dB
+Adrive = 15; %  full scale
 
 %Qualify inputs
 if band <0 | band > 7 
     display('band out of range (for now  0 <= band <= 7')
-    break
+    return
 end
 
-if min(f)<-19.2e6 | max(freqs)>19.2e6
-    display('frequencies must be in range +-19.2MHz')
-    break
+if min(freqs) < -19.2  |  max(freqs) > 19.2
+    display('frequencies must be in range +-19.2 MHz')
+    return
 end
 
 subchan = 16*band; % use channel 0 of this band
 
 lcaPut( [rootPath, 'rfEnable'], 1 ) %enable RF
+lcaPut( [rootPath, 'statusChannelSelect'], subchan)   %set monitor channel to this channel
 
 %loop over frequencies
 for j=1:length(freqs)
-    freqBits = round(2^24*freqs(j)/38.4e6));    %CHECK SCALING!!!
-
     % configCryoChannel( rootPath, subchan, frequency_mhz, amplitude, feedbackEnable, etaPhase, etaMag )
-    configCryoChannel( rootPath, subchan, freqBits, Adrive, 0, 0, 0 )
+    configCryoChannel( rootPath, subchan, freqs(j), Adrive, 0, 0, 0 )
+    pause(dwell);
     for nr = 1:Nread
         %read I & Q, save response as complex vector
+        configCryoChannel( rootPath, subchan, freqs(j), Adrive, 0, 0, 0 ); %write again to trigger status register update
         pause(dwell);
-        configCryoChannel( rootPath, subchan, freqBits, Adrive, 0, 0, 0 ); %write again to trigger status register update
-        resp((j-1)*Nread + nr) = (lcaGet( [pvRoot, 'I']) + 1i*lcaGet( [pvRoot, 'Q'])) / 2^15; %CHECK SCALING!!!
+        resp((j-1)*Nread + nr) = (lcaGet( [rootPath, 'I']) + 1i*lcaGet( [rootPath, 'Q'])) / 2^15; %CHECK SCALING!!!
         % are I&Q synchronous?        
         f((j-1)*Nread + nr) = freqs(j); 
     end
 end
-
+    Adrive = 1; %turn down to very low amplitude
+    configCryoChannel( rootPath, subchan, freqs(j), Adrive, 0, 0, 0 )
 
 % if we should want to check dF:
 % [loopFilterOutput, frequencyError] = readbackCryoChannel( rootPath, channelNum )

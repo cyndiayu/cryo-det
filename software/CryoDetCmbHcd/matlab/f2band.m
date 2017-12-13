2c63833b04b88cecaf903a3a0cf307e5cda56323
cryo <cryo@testbox02.stanford.edu>
2017-12-12 17:50:55 -0800

    decodes frequency into sub-band and offset

--- software/CryoDetCmbHcd/matlab/f2band.m
+++ software/CryoDetCmbHcd/matlab/f2band.m
@@ -2,6 +2,6 @@ function [band, fresid] = f2band(F)
 bandNo = [ 8 24 9 25 10 26 11 27 12 28 13 29 14 30 15 31 0 16 1 17 2 18 3 19 4 20 5 21 6 22 7 23 8];
 bb = floor((F-(5250-307.2-9.6))/19.2);
 fresid = F - (5250 -307.2) - bb*19.2;
-band = bandNo(bb+1)
+band = bandNo(bb+1);
 end
     

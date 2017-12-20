07a7eea67a22b8704c5419e8240728f3ffd34f67
cryo <cryo@testbox02.stanford.edu>
2017-12-20 11:23:35 -0800

    updates as of 20Dec2017

--- software/CryoDetCmbHcd/matlab/testAmplSweepOverlayBands.m
+++ software/CryoDetCmbHcd/matlab/testAmplSweepOverlayBands.m
@@ -1,38 +1,38 @@
 %script testAmplSweep
+%sweeps a full band
+%updated SRS 15Dec2017
 
-Nread = 4
+Nread = 2;
 dwell = 0.02;
-freqs = -14.8:0.2:14.8;   %frequencies in MHz
+freqs = -10:0.2:10;   %frequencies in MHz
 
 resp = zeros(32, size(freqs,2)*Nread);
 f = zeros(32, size(freqs,2)*Nread);
 
-band_centers1 = circshift((0:1:15)-8,8)';
-band_centers2 = circshift((0.5:1:15.5)-8,8)';
+band_centers1 = circshift((0:1:15)'-8, 8);
+band_centers2 = circshift((0.5:1:15.5)'-8, 8);
 band_centers = [band_centers1; band_centers2]*38.4;
+
+figure
+hold on;
+xlabel('Frequency (MHz)')
+ylabel('Amplitude (normalized)')
+title('32 sub-band response')
+
 
 for band=0:31
     disp(['band ' num2str(band)])
     [resp(band+1,:), f(band+1,:)] = amplSweep(band, freqs, Nread, dwell);
     f(band+1,:) = f(band+1,:) + band_centers(band+1);
-end
-
-
-
-
-figure
-for band = 0:15
+    if band == 10
+        xlim([-300 300]);
+    end
     plot(f(band+1,:), abs(resp(band+1,:)), '.', 'color', rand(1,3))
-    hold on
+    grid on;
+
 end
 
-% TODO fix multiply by 2 
-for band = 16:31
-    plot(f(band+1,:), 2*abs(resp(band+1,:)), '.', 'color', rand(1,3))
-    hold on
-end
-
-xlim([-307.2 307.2])
-ylabel('Frequency (MHz)')
-xlabel('Amplitude (normalized)')
+xlim([-300 300])
+xlabel('Frequency (MHz)')
+ylabel('Amplitude (normalized)')
 title('32 sub-band response')

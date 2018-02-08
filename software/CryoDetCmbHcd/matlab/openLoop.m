9f5c8708b0fa397f3ba0a61eb4fe7f4f9154ae8d
cryo <cryo@testbox02.stanford.edu>
2018-02-08 09:53:45 -0800

    Made script a little smarter, now knows how to set for single channel readout and how to read back eta parameters in closed loop before dropping into open loop.

--- software/CryoDetCmbHcd/matlab/openLoop.m
+++ software/CryoDetCmbHcd/matlab/openLoop.m
@@ -1,5 +1,19 @@
-example();
+chan=64;
+lcaPut([rootPath,'readoutChannelSelect'], chan(1));
+lcaPut([rootPath,'singleChannelReadoutOpt2'], 1);
+        
+quickData();
 offset=mean(df);
 disp(['offset=',num2str(offset)]);
 
-configCryoChannel(rootPath, chan(ii), offset(ii), Adrive, 0, etaPhaseDeg(ii), etaScaled(ii));
+pvRoot = [rootPath, 'CryoChannels:CryoChannel[', num2str(chan(1)), ']:'];
+
+ampl=lcaGet([pvRoot,'amplitudeScale']);
+
+etaPhase=lcaGet([pvRoot,'etaPhase']);
+etaPhaseDeg=(etaPhase/2^15)*180;
+
+etaMag=lcaGet([pvRoot,'etaMag']);
+etaScaled=etaMag/2^10;
+
+configCryoChannel(rootPath, chan(1), offset(1), ampl, 0, etaPhaseDeg(1), etaScaled(1));

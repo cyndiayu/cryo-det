058d850027af91474d75c7abc35040e6c3bc2b1b
cryo <cryo@testbox02.stanford.edu>
2018-02-09 11:48:09 -0800

    Script for setting up system after a clia activate/deactivate.  Should move these into the defaults.yml file...someday.

---
+++ software/CryoDetCmbHcd/matlab/setup.m
@@ -0,0 +1,21 @@
+% s
+function setup()
+
+    setEnv('mitch_epics')
+    lcaClear
+    setDefaults
+  
+    root='mitch_epics:AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:';
+    lcaPut([root,'iqSwapIn'], num2str(1));
+    lcaPut([root,'iqSwapOut'], num2str(1));
+    lcaPut([root,'refPhaseDelay'], num2str(5));
+    lcaPut([root,'toneScale'], num2str(2));
+    lcaPut([root,'feedbackGain'], num2str(256));
+    lcaPut([root,'feedbackPolarity'], num2str(1));
+    
+    root='mitch_epics:AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:CryoAdcMux[0]:';
+    lcaPut([root,'ChRemap[0]'] , num2str(6));
+    lcaPut([root,'ChRemap[1]'] , num2str(7));
+
+    readFpgaStatus( root )
+end

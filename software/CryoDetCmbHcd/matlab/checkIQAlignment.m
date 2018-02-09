56e77a7f7ae29f6f8581a64fda0e0e972a0bfc79
cryo <cryo@testbox02.stanford.edu>
2018-02-09 11:07:20 -0800

    Started stupid script to quickly plot what DAC thinks it's sending out and what the ADC is actually receiving, to check for IQ misalignment issue.

---
+++ software/CryoDetCmbHcd/matlab/checkIQAlignment.m
@@ -0,0 +1,18 @@
+% 2.4e6 is downconverted channel rate
+Fadc = 614.4e6;
+rootPath = 'mitch_epics:AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:';
+
+%% What does DAC think it's putting out?
+dacNumber = 0;
+dacData = readDacData( rootPath, dacNumber );
+figure
+pwelch(dacData, [], [], [], Fadc, 'centered')
+title('DAC data PSD')
+
+
+%% check ADC
+adcNumber = 3;
+adcData = readAdcData( rootPath , adcNumber );
+figure
+pwelch(adcData, [], [], [], Fadc, 'centered')
+title('ADC data PSD')

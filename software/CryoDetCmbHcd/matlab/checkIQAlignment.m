28cb61273004d483dba6bfa878880d96cec9bfbc
cryo <cryo@testbox02.stanford.edu>
2018-03-08 15:35:56 -0800

    Added warning to remind user to check that the correct ADC is being read.

--- software/CryoDetCmbHcd/matlab/checkIQAlignment.m
+++ software/CryoDetCmbHcd/matlab/checkIQAlignment.m
@@ -1,3 +1,5 @@
+disp('!!! Remember, must read the correct ADC.  Should match the band # in setup.m');
+
 % 2.4e6 is downconverted channel rate
 Band = 3;
 Fadc = 614.4e6;
@@ -7,15 +9,20 @@ rootPath = 'mitch_epics:AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:';
 dacNumber = Band;
 dacData = readDacData( rootPath, dacNumber );
 figure
-pwelch(dacData, [], [], [], Fadc, 'centered')
+w = blackman(length(adcData));
+pwelch(dacData, w, [], [], Fadc, 'centered')
 title('DAC data PSD')
+
+% figure
+% plot(real(dacData))
 
 
 %% check ADC
 adcNumber = Band;
 adcData = readAdcData( rootPath , adcNumber );
 figure
-pwelch(adcData, [], [], [], Fadc, 'centered')
+w = blackman(length(adcData));
+pwelch(adcData, w, [], [], Fadc, 'centered')
 title('ADC data PSD')
 
 % save data

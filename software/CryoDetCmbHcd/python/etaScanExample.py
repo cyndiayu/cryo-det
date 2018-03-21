87c33a37cebc7c37442df29fabc1d1e859de3213
Mitch D'Ewart <mdewart@slac.stanford.edu>
2018-03-21 14:20:39 -0700

    Update band -> chan

--- software/CryoDetCmbHcd/python/etaScanExample.py
+++ software/CryoDetCmbHcd/python/etaScanExample.py
@@ -1,23 +1,23 @@
 import epics
 import numpy
 
-rootPath     = 'mitch_epics:AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:CryoChannels:'
-etaScanFreqs = epics.PV( rootPath + 'etaScanFreqs' )
-etaScanAmpl  = epics.PV( rootPath + 'etaScanAmplitude' )
+rootPath        = 'mitch_epics:AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:CryoChannels:'
+etaScanFreqs    = epics.PV( rootPath + 'etaScanFreqs' )
+etaScanAmpl     = epics.PV( rootPath + 'etaScanAmplitude' )
 etaScanChannel  = epics.PV( rootPath + 'etaScanChannel' )
-etaScanStart = epics.PV( rootPath + 'runEtaScan' )
+etaScanStart    = epics.PV( rootPath + 'runEtaScan' )
 
 etaScanResultsI = epics.PV( rootPath + 'etaScanResultsReal' )
 etaScanResultsQ = epics.PV( rootPath + 'etaScanResultsImag' )
 
 
 freqs = numpy.arange( -0.3, 0.3, 0.01 )
-band  = 0
+chan  = 0
 ampl  = 10
 
 etaScanFreqs.put( freqs )
 etaScanAmpl.put( ampl )
-etaScanChannel.put( band )
+etaScanChannel.put( chan )
 etaScanStart.put( 1 )
 
 i = etaScanResultsI.get( len( freqs ) )

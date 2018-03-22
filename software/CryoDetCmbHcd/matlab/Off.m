68722fd21a9516a56461b895ea7a740c8e2fd8d0
cryo <cryo@testbox02.stanford.edu>
2018-03-22 11:41:45 -0700

    All channels now set off simultaneously using CryoChannels:setAmplitudeScales register; *much* faster.

--- software/CryoDetCmbHcd/matlab/Off.m
+++ software/CryoDetCmbHcd/matlab/Off.m
@@ -4,13 +4,17 @@
 
 rootPath = 'mitch_epics:AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:';
 
+% Old way, by channel
+%for n=0:511
+%    [band, Freq, ampl] = getChannel(n);
+%    if ampl > 0
+%        display(['found channel ' num2str(n) ' set to amplitude ' num2str( ampl)])
+%        chanPVprefix = [rootPath, 'CryoChannels:CryoChannel[', num2str(n), ']:'];
+%        lcaPut( [chanPVprefix, 'amplitudeScale'], 0) ;
+%        
+%    end
+%end
 
-for n=0:511
-    [band, Freq, ampl] = getChannel(n);
-    if ampl > 0
-        display(['found channel ' num2str(n) ' set to amplitude ' num2str( ampl)])
-        chanPVprefix = [rootPath, 'CryoChannels:CryoChannel[', num2str(n), ']:'];
-        lcaPut( [chanPVprefix, 'amplitudeScale'], 0) ;
-        
-    end
-end
+% New way, turns everyone off efficiently
+chanPVprefix = [rootPath, 'CryoChannels:'];
+lcaPut( [chanPVprefix, 'setAmplitudeScales'], 0);
